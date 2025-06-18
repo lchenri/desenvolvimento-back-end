e164e0c74b089bfb2d30ff3beb37721c0e2a750d
lchenri <lucasbonicardoso@outlook.com>
2025-06-17 21:43:47 -0300

    Added Exception

---
+++ src/main/java/br/com/multicinema/cinemaapi/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package br.com.multicinema.cinemaapi.exception;
+
+public class RegraNegocioException extends RuntimeException{
+    public RegraNegocioException(String message){
+        super(message);
+    }
+}
